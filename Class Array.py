eeb45186c62efe0dcbc202fa11a02273ee4913b0
Mukherjee-89 <138455025+Mukherjee-89@users.noreply.github.com>
2023-07-06 00:16:35 +0530

    Add files via upload

---
+++ Class Array.py
@@ -0,0 +1,10 @@
+class Array:
+    def arr():
+        arr1=[]
+        n=int(input("enter a number : "))
+        for i in range(0,n):
+            ele= int(input())
+            arr1.appendd(ele)
+        print(arr1)
+        print("length of the array is: ",len(arr1))
+Array.arr() 
